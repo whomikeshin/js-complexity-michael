f63e7efa420bf2864da042942a2fb4e8f00b97d4
Mike Shin <michaelkshin@gmail.com>
2016-11-17 17:28:19 -0500

    add handling of switch/case syntax to jsComplexity
    add corresponding tests to jsComplexityTest

--- app/services/js-complexity/jsComplexity.js
+++ app/services/js-complexity/jsComplexity.js
@@ -2,12 +2,34 @@
 
 angular.module('myApp')
 .service('jsComplexity', function() {
+  var keywords = [
+    'if', 'else if', 'else'
+  ];
+  // custom parser
+
   function countIfs(jsCode) {
-    return jsCode.match(/(if(?!'|"))|(else if(?!'|"))|(else(?!'|"))/gi).length;
+    var matches = jsCode.match(/(if(?!'|"))|(else if(?!'|"))|(else(?!'|"))/gi);
     // match only if not followed by ' or "
+
+    if (matches) {
+      return matches.length;
+    }
+    return 0;
+  }
+
+  function countCases(jsCode) {
+    var matches = jsCode.match(/(case(?!'|"))|(default(?!'|"))/gi);
+
+    if (matches) {
+      return matches.length;
+    }
+    return 0;
   }
 
   this.evaluate = function(jsCode) {
-    return countIfs(jsCode);
+    var ifCount = countIfs(jsCode),
+        caseCount = countCases(jsCode);
+
+    return ifCount + caseCount;
   };
 });
--- app/services/js-complexity/jsComplexityTest.js
+++ app/services/js-complexity/jsComplexityTest.js
@@ -10,15 +10,15 @@ describe('myApp.jsComplexity Service', function() {
       expect(jsComplexity).toBeDefined();
       expect(jsComplexity.evaluate).toBeDefined();
     }));
-  
+
     it('should evaluate a single if correctly', inject(function(jsComplexity) {
        expect(jsComplexity.evaluate('function check(a){ if(a){return a;}}')).toEqual(1);
     }));
-  
+
     it('should evaluate an if, else correctly', inject(function(jsComplexity) {
       expect(jsComplexity.evaluate('function check(a){ if(a){return a;}else{return 0;}}')).toEqual(2);
     }));
-  
+
     it('should evaluate an if, else if, else correctly', inject(function(jsComplexity) {
       expect(jsComplexity.evaluate('function check(a){ if(a=1){return a;}else if(a=0){return 2;}else{return 0;}}')).toEqual(3);
     }));
@@ -30,6 +30,17 @@ describe('myApp.jsComplexity Service', function() {
     it('should ignore conditions in strings', inject(function(jsComplexity) {
       expect(jsComplexity.evaluate('function check(a){ if(a=1){return "if";}else{return "else";}}')).toEqual(2);
     }));
+
+    it('should evaluate a switch statement with only cases correctly', inject(function(jsComplexity) {
+      expect(jsComplexity.evaluate('function switch(a){ case(a=0):return a;break; case(a=1):return a;break;')).toEqual(2);
+    }));
+
+    it('should evaluate a switch statement with cases and default correctly', inject(function(jsComplexity) {
+      expect(jsComplexity.evaluate('function switch(a){ case(a=0):return a;break; case(a=1):return a;break; default:return -1;}')).toEqual(3);
+    }));
+
+    it('should ignore switch conditions in strings', inject(function(jsComplexity) {
+      expect(jsComplexity.evaluate('function switch(a){ case(a=1):return "case";break; default:return "default";}')).toEqual(2);
+    }));
   });
 });
-  
