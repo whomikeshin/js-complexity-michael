95dd6a77a60601217df6eadcafb9202b1cc865b0
Mike Shin <michaelkshin@gmail.com>
2016-11-18 10:35:49 -0500

    refactor count functions in jsComplexity

--- app/mainView/mainViewTest.js
+++ app/mainView/mainViewTest.js
@@ -1,16 +1,16 @@
-// 'use strict';
-//
-// describe('myApp module', function() {
-//
-//   beforeEach(module('myApp'));
-//
-//   describe('mainView controller', function(){
-//
-//     it('should ....', inject(function($controller) {
-//       //spec body
-//       var mainViewCtrl = $controller('mainViewCtrl');
-//       expect(mainViewCtrl).toBeDefined();
-//     }));
-//
-//   });
-// });
+'use strict';
+
+describe('myApp module', function() {
+
+  beforeEach(module('myApp'));
+
+  describe('mainViewCtrl Controller', function(){
+  //
+    it('should ....', inject(function($controller) {
+  //   //   //spec body
+      var mainViewCtrl = $controller('mainViewCtrl');
+      expect(mainViewCtrl).toBeDefined();
+    }));
+  //
+  });
+});
--- app/services/js-complexity/jsComplexity.js
+++ app/services/js-complexity/jsComplexity.js
@@ -2,62 +2,35 @@
 
 angular.module('myApp')
 .service('jsComplexity', function() {
-  var keywords = [
-    'if', 'else if', 'else'
-  ];
-  // custom parser
 
   function countIfs(jsCode) {
-    // match only if not followed by ' or "
-    // delete else regex
-    // case where "do this or else "
-    var matches = jsCode.match(/(if(?!'|"))|(else if(?!'|"))/g);
+    var matches = jsCode.match(/(if(?!'|"))|(else if(?!'|"))/gi);
 
-    if (matches) {
-      return matches.length;
-    }
-    return 0;
+    return matches ? matches.length : 0;
   }
 
   function countCases(jsCode) {
-    // delete default regex
-    var matches = jsCode.match(/(case(?!'|"))/g);
+    var matches = jsCode.match(/(case(?!'|"))/gi);
 
-    if (matches) {
-      return matches.length;
-    }
-    return 0;
+    return matches ? matches.length : 0;
   }
 
   function countLoops(jsCode) {
     var matches = jsCode.match(/(while(?!'|"))|(for(?!'|"))/gi);
 
-    if (matches) {
-      return matches.length;
-    }
-
-    return 0;
+    return matches ? matches.length : 0;
   }
 
   function countTernary(jsCode) {
-    // how to match ?
     var matches = jsCode.match(/([?](?=(.*[:])))/g);
 
-    if (matches) {
-      return matches.length;
-    }
-
-    return 0;
+    return matches ? matches.length : 0;
   }
 
   function countOrs(jsCode) {
     var matches = jsCode.match(/([|](?!'|"))/g);
 
-    if (matches) {
-      return Math.floor(matches.length / 2);
-    }
-
-    return 0;
+    return matches ? Math.floor(matches.length / 2) : 0;
   }
 
   this.evaluate = function(jsCode) {
@@ -67,7 +40,7 @@ angular.module('myApp')
         ternaryCount = countTernary(jsCode),
         orCount = countOrs(jsCode);
 
-    return ifCount + caseCount + loopCount + ternaryCount + orCount + 1;
     // complexity equals 1 if source code has no control flow statements
+    return 1 + ifCount + caseCount + loopCount + ternaryCount + orCount;
   };
 });
