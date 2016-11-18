658ac4b1eba500c3ea4cf252d73a23e236b4501d
Mike Shin <michaelkshin@gmail.com>
2016-11-17 22:02:00 -0500

    add handling of ternary operator
    add ternary tests(2) to spec

--- app/services/js-complexity/jsComplexity.js
+++ app/services/js-complexity/jsComplexity.js
@@ -10,6 +10,7 @@ angular.module('myApp')
   function countIfs(jsCode) {
     // match only if not followed by ' or "
     // delete else regex
+    // case where "do this or else "
     var matches = jsCode.match(/(if(?!'|"))|(else if(?!'|"))/gi);
 
     if (matches) {
@@ -38,12 +39,35 @@ angular.module('myApp')
     return 0;
   }
 
+  function countTernary(jsCode) {
+    // how to match ?
+    var matches = jsCode.match(/([?](?=(.*[:])))/gi);
+
+    if (matches) {
+      return matches.length;
+    }
+
+    return 0;
+  }
+
+  // function countOrs(jsCode) {
+  //   var matches = jsCode.match(/([||](?!'|"))/gi);
+  //
+  //   if (matches) {
+  //     return matches.length;
+  //   }
+  //
+  //   return 0;
+  // }
+
   this.evaluate = function(jsCode) {
     var ifCount = countIfs(jsCode),
         caseCount = countCases(jsCode),
-        loopCount = countLoops(jsCode);
+        loopCount = countLoops(jsCode),
+        ternaryCount = countTernary(jsCode);
+        // orCount = countOrs(jsCode);
 
-    return ifCount + caseCount + loopCount + 1;
+    return ifCount + caseCount + loopCount + ternaryCount + 1;
     // complexity equals 1 if source code has no control flow statements
   };
 });
--- app/services/js-complexity/jsComplexityTest.js
+++ app/services/js-complexity/jsComplexityTest.js
@@ -67,5 +67,15 @@ describe('myApp.jsComplexity Service', function() {
     it('should evaluate a while loop with nested if, else if, else correctly', inject(function(jsComplexity) {
       expect(jsComplexity.evaluate('function check(a){ while(a>0){if(a<2){a-=1;}else if(a<10){a/=2;}else{a/=5;}}}')).toEqual(4);
     }));
+
+    // Tenary
+    it('should evaluate a ternary operator correctly', inject(function(jsComplexity) {
+      expect(jsComplexity.evaluate('function check(a){ return a%2===0?"even":"odd";}')).toEqual(2);
+    }));
+
+    it('should ignore ternary operator in strings', inject(function(jsComplexity) {
+      expect(jsComplexity.evaluate('function check(a){ return a%2===0?"even":"?";}')).toEqual(2);
+    }));
+
   });
 });
